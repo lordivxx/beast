ff22b3785c58ca15e3d3a2828c81c1e68fd8c00b
Chuck Forsyth <forsythc@ucr.edu>
2017-11-11 14:47:06 -0800

    developing

--- beast.py
+++ beast.py
@@ -9,6 +9,7 @@ from subprocess import Popen, PIPE
 import time
 import os
 import sys
+import random
 import argparse
 import ConfigParser
 
@@ -26,8 +27,20 @@ charactercfg = args.charactercfg
 
 
 ######## Functions #########
+def attack(hp,atk,df):
+    while hp > 0:
+        hp = hp - random.randint(1, int(atk))
+        print(hp)
+
+
+def fight(bhp,batk,mhp,matk):
+    while bhp > 0 and mhp > 0:
+        bhp = int(bhp) - random.randint(1, int(matk))
+        mhp = int(mhp) - random.randint(1, int(batk))
+        print (bhp,mhp)
+
 def logthis(name,value):
-    logname = 'beast_{0}.log'.format(int(starttime))
+    logname = 'log/{0}.log'.format(int(starttime))
     logdata = '{0}:{1}'.format(str(name), str(value))
     f=open(logname, "a+")
     f.write("%s\r\n" % (logdata))
@@ -57,7 +70,13 @@ if __name__ == '__main__':
     ## Reads in the config file that was givin and stores some values from it
     Config = ConfigParser.ConfigParser()
     Config.read(charactercfg)
-    print(Config.sections())
-    print(Config.options("Main"))
+    for options in Config.sections():
+        logthis('options', Config.options(options))
     beast_name = ConfigSectionMap("Main")['name']
-    print(beast_name)
+    beast_hp = ConfigSectionMap("Main")['hp']
+    beast_atk = ConfigSectionMap("Main")['atk']
+    beast_def = ConfigSectionMap("Main")['def']
+    print('Welcome Back {0}'.format(str(beast_name)))
+    print('You have {0} HP, {1} Attack Power, and {2} Defence.'.format(str(beast_hp),str(beast_atk),str(beast_def)))
+    #attack(int(beast_hp),int(beast_atk),int(beast_def))
+    fight(beast_hp,beast_atk,100,2)
