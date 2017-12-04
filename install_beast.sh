091c0ac78bf34e882a04442c190d99ade9114be7
Chuck Forsyth <forsythc@ucr.edu>
2017-12-03 22:49:08 -0800

    new install file

---
+++ install_beast.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf $HOME/repos/beast
+mkdir -p $HOME/repos/
+cd $HOME/repos/
+git clone https://github.com/lordivxx/beast.git
+
+#echo "export PATH=$HOME/repos/beast:$PATH" >> $HOME/.bashrc
